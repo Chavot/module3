beff551f86702151cdde12c9e36f5d05b1d79136
Chavot <ukuharm@gmail.com>
2022-11-28 15:34:37 +0200

    inital commit and tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+    ],
+    theme: {
+        colors: {
+            'blue': '#3C787E',
+            'light-brown': '#3C787E',
+            'brown': '#D56F3E',
+            'black': '#030301',
+            'purple': '#49306B',
+            'white': '#D5DCF9',
+        },
+        extend: {},
+    },
+    plugins: [],
+}
